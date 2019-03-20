8ae21da358e901f1079ccfee07fa2550429e5fcd
XxshiftxX <qjfrntop12@naver.com>
2019-03-20 09:12:40 +0900

    Config ts

---
+++ dist/test/test.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const assert = __importStar(require("assert"));
+describe('Nothing.', () => {
+    it("asdf", () => {
+        assert.equal(-1, -1);
+    });
+});
---
+++ src/test/test.ts
@@ -0,0 +1,7 @@
+import * as assert from 'assert';
+
+describe('Nothing.', () => {
+    it("asdf", () => {
+        assert.equal(-1, -1);
+    })
+})
