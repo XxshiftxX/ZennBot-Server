1e3833624e80c6ba3b3c7cb70e987fbaf6b966df
XxshiftxX <qjfrntop12@naver.com>
2019-03-27 15:57:07 +0900

    Set database for travis-ci

--- models/index.js
+++ models/index.js
@@ -4,6 +4,8 @@ const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
 const db = {};
 
+console.log(`ENV >> ${env}`)
+
 const sequelize = new Sequelize(
   config.database, config.username, config.password, config,
 );
