b680972f9888d2886f67f8f897486de72687d2e0
XxshiftxX <qjfrntop12@naver.com>
2019-05-24 21:50:44 +0900

    Add getPoint function in model

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,4 +3,14 @@ import { config } from './config';
 
 const connection = createConnection(config);
 
-connection.connect();
+export const getPoint = (tname) => new Promise((resolve, reject) => {
+  connection.query(`SELECT ticket, piece from users where tname="${tname}"`, (err, low) => {
+    if (err || low.length == 0) {
+      reject(err);
+      return;
+    }
+    
+    const { ticket, piece } = low[0];
+    resolve({ ticket, piece });
+  });
+});
