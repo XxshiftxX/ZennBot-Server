dd745a1a0520acd52bd938701828a858244fc4fa
XxshiftxX <qjfrntop12@naver.com>
2019-05-27 10:52:58 +0900

    Add getTnameByTdisplay function on model

--- src/models/index.ts
+++ src/models/index.ts
@@ -20,8 +20,25 @@ export const getPoint = (tname) => new Promise<any>((resolve, reject) => {
   });
 });
 
-export const updatePoint = (tname: string, type: string, amount: Number = 1) => new Promise(async (resolve, reject) => {
+export const getTnameByTdisplay = (tdisplay: string) => new Promise<string>((resolve, reject) => {
+  connection.query(`SELECT tname from users where tdisplay="${tdisplay}"`, (err, low) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    if (low.length == 0) {
+      reject(new NotFoundError(`Twitch display name "${tdisplay}" not found from users database`));
+      return;
+    }
+
+    resolve(low[0].tname);
+  });
+});
+
+export const updatePoint = (tname: string, type: string, amount: Number) => new Promise(async (resolve, reject) => {
   const point = await getPoint(tname);
+  if (amount == undefined)
+    amount = 0;
   connection.query(`UPDATE users SET ${type}=${point[type]+amount} WHERE tname="${tname}"`, (err) => {
     if (err) {
       reject(err);
